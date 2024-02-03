ff496f963a6d1ffdacc90ca73200f16b201dbc8b
Christpher Oka <chriskroka@gmail.com>
2024-02-03 23:09:56 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,214 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                https://vrjpqtdp.api.sanity.io/v2022-03-07/data/query/production?query=*[_type
+                == "article"]
+            </div>
+            <pre>
+                {JSON.stringify(
+                    {
+                        query: '*[_type == "article"]\n\n',
+                        result: [
+                            {
+                                title: "Test 2",
+                                _updatedAt: "2024-02-03T03:45:58Z",
+                                _createdAt: "2024-02-03T03:45:58Z",
+                                _rev: "8mGThtWzkZE49nEaO4U59w",
+                                _type: "article",
+                                _id: "1f2fa315-f37b-4dc0-882b-fb62a4332f90",
+                            },
+                            {
+                                slug: {
+                                    current: "test-article",
+                                    _type: "slug",
+                                },
+                                _createdAt: "2024-02-03T02:57:14Z",
+                                _rev: "MkmgsAGxwT6Nx7jjOeWYUZ",
+                                _type: "article",
+                                _id: "59f0cee0-157a-456d-a838-c9d1507e8c68",
+                                body: [
+                                    {
+                                        children: [
+                                            {
+                                                _type: "span",
+                                                marks: [],
+                                                text: "lajsdlfjl;asjdfl;kjf",
+                                                _key: "0ee5279232ae",
+                                            },
+                                        ],
+                                        _type: "block",
+                                        style: "normal",
+                                        _key: "0ed1f180a75c",
+                                        markDefs: [],
+                                    },
+                                    {
+                                        _type: "block",
+                                        style: "normal",
+                                        _key: "149edecfcd9f",
+                                        markDefs: [],
+                                        children: [
+                                            {
+                                                _type: "span",
+                                                marks: [],
+                                                text: "",
+                                                _key: "45da5b999c15",
+                                            },
+                                        ],
+                                    },
+                                    {
+                                        _type: "image",
+                                        _key: "e79afa9a90a2",
+                                        asset: {
+                                            _ref: "image-92682811953395af7c3745da67b21f45694fc703-462x540-png",
+                                            _type: "reference",
+                                        },
+                                    },
+                                    {
+                                        style: "normal",
+                                        _key: "207f45e64067",
+                                        markDefs: [],
+                                        children: [
+                                            {
+                                                _type: "span",
+                                                marks: [],
+                                                text: "lajsdf;lkjasdkf",
+                                                _key: "f59e1238872e",
+                                            },
+                                        ],
+                                        _type: "block",
+                                    },
+                                    {
+                                        _type: "block",
+                                        style: "normal",
+                                        _key: "3cddd095d95d",
+                                        markDefs: [],
+                                        children: [
+                                            {
+                                                _key: "532e08446f07",
+                                                _type: "span",
+                                                marks: [],
+                                                text: "alsjdfljdsa;kf",
+                                            },
+                                        ],
+                                    },
+                                    {
+                                        children: [
+                                            {
+                                                _type: "span",
+                                                marks: [],
+                                                text: "alksdjfljkasdf",
+                                                _key: "31396d967d53",
+                                            },
+                                        ],
+                                        _type: "block",
+                                        style: "normal",
+                                        _key: "4408b8f7af15",
+                                        markDefs: [],
+                                    },
+                                    {
+                                        _type: "image",
+                                        _key: "91aaca6bc7bd",
+                                    },
+                                    {
+                                        _type: "block",
+                                        style: "normal",
+                                        _key: "ab8ab8181671",
+                                        markDefs: [],
+                                        children: [
+                                            {
+                                                _type: "span",
+                                                marks: [],
+                                                text: "jasdl;kjf",
+                                                _key: "72e6d79589b0",
+                                            },
+                                        ],
+                                    },
+                                    {
+                                        markDefs: [],
+                                        children: [
+                                            {
+                                                text: "",
+                                                _key: "35f7ab214a7a",
+                                                _type: "span",
+                                                marks: [],
+                                            },
+                                        ],
+                                        _type: "block",
+                                        style: "normal",
+                                        _key: "e672e1a2c739",
+                                    },
+                                    {
+                                        _key: "1c8a1bc41aa2",
+                                        markDefs: [],
+                                        children: [
+                                            {
+                                                marks: [],
+                                                text: "asdfj;sjadf",
+                                                _key: "27e82266aa52",
+                                                _type: "span",
+                                            },
+                                        ],
+                                        _type: "block",
+                                        style: "normal",
+                                    },
+                                    {
+                                        _type: "image",
+                                        _key: "723eb334b1c3",
+                                    },
+                                    {
+                                        markDefs: [],
+                                        children: [
+                                            {
+                                                _type: "span",
+                                                marks: [],
+                                                text: ";alskdjflksjadf",
+                                                _key: "f3e75b5e43b6",
+                                            },
+                                        ],
+                                        _type: "block",
+                                        style: "normal",
+                                        _key: "6048bc9730af",
+                                    },
+                                    {
+                                        children: [
+                                            {
+                                                _type: "span",
+                                                marks: [],
+                                                text: "",
+                                                _key: "d36028768328",
+                                            },
+                                        ],
+                                        _type: "block",
+                                        style: "normal",
+                                        _key: "5035904d4edc",
+                                        markDefs: [],
+                                    },
+                                    {
+                                        _key: "1e41ea3a6e58",
+                                        markDefs: [],
+                                        children: [
+                                            {
+                                                _type: "span",
+                                                marks: [],
+                                                text: "",
+                                                _key: "5a0f45c951ce",
+                                            },
+                                        ],
+                                        _type: "block",
+                                        style: "normal",
+                                    },
+                                ],
+                                title: "Test Article",
+                                _updatedAt: "2024-02-03T03:36:12Z",
+                            },
+                        ],
+                        ms: 4,
+                    },
+                    undefined,
+                    2
+                )}
+            </pre>
+        </main>
+    );
 }
